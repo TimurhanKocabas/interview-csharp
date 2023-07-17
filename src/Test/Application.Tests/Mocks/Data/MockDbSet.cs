044f2a4b0bb5c457a61e66d6ceb8ba0e682a14cb
timurhan kocabas <timurhankocabas@outlook.com>
2023-07-17 11:21:18 +0200

    add: MockDbSet for dbcontext datasets.>List<entity> edits: async query needs more implementation and changed to sync expected results fixed.

---
+++ src/Test/Application.Tests/Mocks/Data/MockDbSet.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Moq;
+
+namespace Application.Tests.Mocks.Data;
+internal class MockDbSet<TEntity> : Mock<DbSet<TEntity>> where TEntity : class
+{
+    public MockDbSet(List<TEntity> dataSource = null)
+    {
+        var data = (dataSource ?? new List<TEntity>());
+        var queryable = data.AsQueryable();
+
+        this.As<IQueryable<TEntity>>().Setup(e => e.Provider).Returns(queryable.Provider);
+        this.As<IQueryable<TEntity>>().Setup(e => e.Expression).Returns(queryable.Expression);
+        this.As<IQueryable<TEntity>>().Setup(e => e.ElementType).Returns(queryable.ElementType);
+        this.As<IQueryable<TEntity>>().Setup(e => e.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+        //Mocking the insertion of entities
+        this.Setup(_ => _.Add(It.IsAny<TEntity>())).Returns((TEntity arg) =>
+        {
+            data.Add(arg);
+            return arg as EntityEntry<TEntity>;
+        });
+
+        //...the same can be done for other members like Remove
+    }
+}
--- src/Test/Application.Tests/Url/Commands/CreateShortUrlCommandTest.cs
+++ src/Test/Application.Tests/Url/Commands/CreateShortUrlCommandTest.cs
@@ -1,5 +1,7 @@
 ﻿namespace Application.Tests.Url.Commands;
 
+using System.Reflection.Metadata;
+using Application.Tests.Mocks.Data;
 using HashidsNet;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -11,12 +13,15 @@ public class CreateShortUrlCommandTest
 {
     private Mock<Hashids> hashIds { get; set; }
     private Mock<IApplicationDbContext> context { get; set; }
-    private readonly string ServerAddress = "https://localhost:7072";
+    private readonly string ServerAddress = "https://localhost:7072/u";
     public CreateShortUrlCommandTest()
     {
         //TODO: Better to set DI for environment for Hashids Configuration
+        var mockSet = new MockDbSet<Url>();
+
         context = new Mock<IApplicationDbContext>();
-        context.Setup(t => t.Urls).Returns(new Mock<DbSet<Url>>().Object);
+        context.Setup(t => t.Urls).Returns(mockSet.Object);
+
         hashIds = new Mock<Hashids>("LPMUtoRmwaRvEosIo", 6, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", "cfhistuCFHISTU");
 
     }
@@ -25,16 +30,18 @@ public class CreateShortUrlCommandTest
     public async Task DoesCreateShortUrlCommandHandlerWork()
     {
         //Arrange
+
         CreateShortUrlCommand command = new CreateShortUrlCommand()
         {
-            Url = "https://www.enpal.de/"
+            Url = "https://www.enpal.de/",
+            BaseReturnUrl = ServerAddress
         };
         CreateShortUrlCommandHandler createShortUrlCommandHandler = new CreateShortUrlCommandHandler(context.Object, hashIds.Object);
         //Act
         var resultFromHandler = await createShortUrlCommandHandler.Handle(command, CancellationToken.None);
         //Arrange
-        var urlEntityFromDb = await context.Object.Urls.SingleOrDefaultAsync(t => t.OriginalUrl == command.Url);
-        string expectedResult = ServerAddress + hashIds.Object.EncodeLong(urlEntityFromDb?.Id ?? 0);
+        var urlEntityFromDb = context.Object.Urls.SingleOrDefault(t => t.OriginalUrl == command.Url);
+        string expectedResult = ServerAddress + "/" + hashIds.Object.EncodeLong(urlEntityFromDb?.Id ?? 0);
         //Assert
         Assert.Equal(expectedResult, resultFromHandler);
     }
